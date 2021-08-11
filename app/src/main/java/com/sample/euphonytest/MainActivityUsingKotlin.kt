d5fbddbae3f88771ae6c811e87ba76b6dd3b1bc9
김효린 <hyorin97@naver.com>
2021-08-12 00:24:57 +0900

    feat: Add sound transmitter using euphony library using Kotlin

---
+++ app/src/main/java/com/sample/euphonytest/MainActivityUsingKotlin.kt
@@ -0,0 +1,55 @@
+package com.sample.euphonytest
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import euphony.lib.transmitter.EuTxManager
+
+class MainActivityUsingKotlin : AppCompatActivity() {
+
+    private lateinit var mTxManager: EuTxManager
+    private var isSpeakOn = false
+    private lateinit var soundControlButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        transmitterSetting()
+
+        soundControlButton = findViewById(R.id.button_sound_control)
+        soundControlButton.setOnClickListener(soundButtonClickListener())
+
+        // layout initial setting
+        soundOff()
+    }
+
+    private fun transmitterSetting() {
+        mTxManager = EuTxManager()
+    }
+
+    private fun soundButtonClickListener(): View.OnClickListener {
+        return View.OnClickListener { view: View? -> changeSoundStatus() }
+    }
+
+    private fun changeSoundStatus() {
+        when (isSpeakOn) {
+            true -> soundOff()
+            false -> soundOn()
+        }
+    }
+
+    private fun soundOn() {
+        mTxManager.euInitTransmit("Hello, Euphony") // To generate acoustic data "Hello, Euphony"
+        mTxManager.process(-1) // generate sound infinite.
+        soundControlButton.text = "사운드 On"
+        isSpeakOn = true
+    }
+
+    private fun soundOff() {
+        mTxManager.stop()
+        soundControlButton.text = "사운드 Off"
+        isSpeakOn = false
+    }
+}
